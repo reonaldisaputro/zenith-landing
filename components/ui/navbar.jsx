300085fceb70fd5285c9893f56371099afe6b795
reonaldi152 <reonaldi@team.superfk.co>
2025-02-19 19:49:52 +0700

    add navbar

--- components/ui/navbar.jsx
+++ components/ui/navbar.jsx
@@ -6,17 +6,24 @@ import { useTranslations } from "next-intl";
 
 export default function Navbar() {
   const t = useTranslations("Navbar");
+
   return (
     <header className="section-header bg-black sticky top-0 left-0 right-0 z-30 py-3">
-      <div className="container">
+      <div className="container mx-auto px-6">
         <div className="flex items-center justify-between">
-          <Link href={"/"} className="text-secondary font-bold text-2xl">
+          <Link href="/" className="text-secondary font-bold text-2xl">
             Mifta Digital Solution
           </Link>
-          <div className="flex items-center gap-6">
+          <nav className="flex items-center gap-6">
+            <Link
+              href="/en/portfolio"
+              className="text-white hover:text-yellow-400 transition"
+            >
+              {t("portfolio")}
+            </Link>
             <LangSwitcher />
             <Button>{t("contact")}</Button>
-          </div>
+          </nav>
         </div>
       </div>
     </header>
