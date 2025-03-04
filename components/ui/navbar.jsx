5c3e83cda0ac605fc94015c37cdbcc5d9d4e2ee1
reonaldi152 <reonaldi@team.superfk.co>
2025-03-04 19:57:10 +0700

    Revert "add navbar"
    This reverts commit 300085fceb70fd5285c9893f56371099afe6b795.

--- components/ui/navbar.jsx
+++ components/ui/navbar.jsx
@@ -6,24 +6,17 @@ import { useTranslations } from "next-intl";
 
 export default function Navbar() {
   const t = useTranslations("Navbar");
-
   return (
     <header className="section-header bg-black sticky top-0 left-0 right-0 z-30 py-3">
-      <div className="container mx-auto px-6">
+      <div className="container">
         <div className="flex items-center justify-between">
-          <Link href="/" className="text-secondary font-bold text-2xl">
+          <Link href={"/"} className="text-secondary font-bold text-2xl">
             Mifta Digital Solution
           </Link>
-          <nav className="flex items-center gap-6">
-            <Link
-              href="/en/portfolio"
-              className="text-white hover:text-yellow-400 transition"
-            >
-              {t("portfolio")}
-            </Link>
+          <div className="flex items-center gap-6">
             <LangSwitcher />
             <Button>{t("contact")}</Button>
-          </nav>
+          </div>
         </div>
       </div>
     </header>
